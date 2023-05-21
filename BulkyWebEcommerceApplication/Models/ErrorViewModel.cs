81d08cfbc76395eedb31544ece92753b13f0ae1a
divyanshijain <divyanshi.jain@nagarro.com>
2023-05-21 18:48:30 +0530

    Add project files.

---
+++ BulkyWebEcommerceApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BulkyWebEcommerceApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
